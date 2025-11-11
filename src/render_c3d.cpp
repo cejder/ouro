d14c5ee3251b92ade93565503a31cc8e03db7328
Celal Ejder <celalejder@gmail.com>
2025-11-11 13:52:00 +0100

    update

--- src/render_c3d.cpp
+++ src/render_c3d.cpp
@@ -106,8 +106,8 @@ void c3d_pull_default_to_other(Camera3D *src) {
     default_camera->target   = src->target;
 }
 
-void static i_calculate_frustum_planes(Vector4 frustum_planes[RENDER_FRUSTUM_PLANE_COUNT]) {
-    // Extract frustum planes
+void static i_calculate_frustum_planes(Vector4 frustum_planes[RENDER_FRUSTUM_PLANE_COUNT], BOOL is_ortho) {
+    // Extract frustum planes using Gribb-Hartmann method
     // Left plane
     frustum_planes[0] = Vector4{
         g_render.cameras.c3d.mat_view_proj.m3 + g_render.cameras.c3d.mat_view_proj.m0,
@@ -136,13 +136,26 @@ void static i_calculate_frustum_planes(Vector4 frustum_planes[RENDER_FRUSTUM_PLA
         g_render.cameras.c3d.mat_view_proj.m11 + g_render.cameras.c3d.mat_view_proj.m9,
         g_render.cameras.c3d.mat_view_proj.m15 + g_render.cameras.c3d.mat_view_proj.m13,
     };
-    // Near plane
-    frustum_planes[4] = Vector4{
-        g_render.cameras.c3d.mat_view_proj.m2,
-        g_render.cameras.c3d.mat_view_proj.m6,
-        g_render.cameras.c3d.mat_view_proj.m10,
-        g_render.cameras.c3d.mat_view_proj.m14,
-    };
+
+    // Near and far planes differ between perspective and orthographic
+    if (is_ortho) {
+        // For orthographic, near plane is m3 + m2
+        frustum_planes[4] = Vector4{
+            g_render.cameras.c3d.mat_view_proj.m3 + g_render.cameras.c3d.mat_view_proj.m2,
+            g_render.cameras.c3d.mat_view_proj.m7 + g_render.cameras.c3d.mat_view_proj.m6,
+            g_render.cameras.c3d.mat_view_proj.m11 + g_render.cameras.c3d.mat_view_proj.m10,
+            g_render.cameras.c3d.mat_view_proj.m15 + g_render.cameras.c3d.mat_view_proj.m14,
+        };
+    } else {
+        // For perspective, near plane is just m2
+        frustum_planes[4] = Vector4{
+            g_render.cameras.c3d.mat_view_proj.m2,
+            g_render.cameras.c3d.mat_view_proj.m6,
+            g_render.cameras.c3d.mat_view_proj.m10,
+            g_render.cameras.c3d.mat_view_proj.m14,
+        };
+    }
+
     // Far plane
     frustum_planes[5] = Vector4{
         g_render.cameras.c3d.mat_view_proj.m3 - g_render.cameras.c3d.mat_view_proj.m2,
@@ -165,9 +178,22 @@ void c3d_update_frustum() {
     Camera3D *cam                      = g_render.cameras.c3d.active_cam;
     g_render.cameras.c3d.fov_radians   = cam->fovy * DEG2RAD;
     g_render.cameras.c3d.mat_view      = MatrixLookAt(cam->position, cam->target, cam->up);
-    g_render.cameras.c3d.mat_proj      = MatrixPerspective(g_render.cameras.c3d.fov_radians, g_render.aspect_ratio, RENDER_NEAR_PLANE, RENDER_FAR_PLANE);
+
+    // Use appropriate projection matrix based on camera projection type
+    if (cam->projection == CAMERA_ORTHOGRAPHIC) {
+        // For orthographic projection, fovy represents the total visible height
+        // We need to calculate the view width based on aspect ratio
+        F32 const top    = cam->fovy / 2.0F;
+        F32 const bottom = -top;
+        F32 const right  = top * g_render.aspect_ratio;
+        F32 const left   = -right;
+        g_render.cameras.c3d.mat_proj = MatrixOrtho(left, right, bottom, top, RENDER_NEAR_PLANE, RENDER_FAR_PLANE);
+    } else {
+        g_render.cameras.c3d.mat_proj = MatrixPerspective(g_render.cameras.c3d.fov_radians, g_render.aspect_ratio, RENDER_NEAR_PLANE, RENDER_FAR_PLANE);
+    }
+
     g_render.cameras.c3d.mat_view_proj = MatrixMultiply(g_render.cameras.c3d.mat_view, g_render.cameras.c3d.mat_proj);
-    i_calculate_frustum_planes(g_render.cameras.c3d.frustum_planes);
+    i_calculate_frustum_planes(g_render.cameras.c3d.frustum_planes, cam->projection == CAMERA_ORTHOGRAPHIC);
 
     for (SZ i = 0; i < RENDER_FRUSTUM_PLANE_COUNT; ++i) {
         g_render.cameras.c3d.normals[i] = {
@@ -188,13 +214,16 @@ BOOL c3d_is_point_in_frustum(Vector3 point) {
 }
 
 BOOL c3d_is_obb_in_frustum(OrientedBoundingBox bbox) {
+    Camera3D *cam = g_render.cameras.c3d.active_cam;
+
+    // Perspective frustum culling using separating axis theorem
     for (SZ i = 0; i < RENDER_FRUSTUM_PLANE_COUNT; ++i) {
         Vector3 const normal = g_render.cameras.c3d.normals[i];
 
         // Project OBB radius onto plane normal
         F32 const r = (bbox.extents.x * math_abs_f32(Vector3DotProduct(bbox.axes[0], normal))) +
-                      (bbox.extents.y * math_abs_f32(Vector3DotProduct(bbox.axes[1], normal))) +
-                      (bbox.extents.z * math_abs_f32(Vector3DotProduct(bbox.axes[2], normal)));
+            (bbox.extents.y * math_abs_f32(Vector3DotProduct(bbox.axes[1], normal))) +
+            (bbox.extents.z * math_abs_f32(Vector3DotProduct(bbox.axes[2], normal)));
 
         // Distance from center to plane
         F32 const d = Vector3DotProduct(normal, bbox.center) + g_render.cameras.c3d.frustum_planes[i].w;
